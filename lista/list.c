fa47a55aacd1228a8513b530ab9ca7a5e44b9db4
Nathalia Moniz Cordova <nathaliamonizcordova@gmail.com>
2024-04-06 11:47:55 +0200

    update list.c

--- lista/list.c
+++ lista/list.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include "list.h"
+#include <pthread.h>
 
+pthread_mutex_t mutex_lista1, mutex_lista2;
 
 int iniciar(List *l) {
 	*l = NULL;
@@ -11,31 +13,42 @@ int iniciar(List *l) {
 
 int set(List *l, int key, char *value1, int N_value2, double *V_value2){
 	
+	pthread_mutex_lock(&mutex_lista1); // Para proteger la verificación de la lista
 	struct Node *ptr, *temp;
 	temp = *l;
 	
 	// El vector no tiene un tamaño permitido
 	if (N_value2 > 32 || N_value2 < 1){
 		printf("Error: el número de valores del vector solo puede estar entre 1 y 32\n");
-		
+		pthread_mutex_unlock(&mutex_lista1);
 		return -1;
 	}
 
 	while (temp != NULL) {
         if (temp->key == key) {
             printf("Error: Key %d ya está en la lista.\n", key);
-			
+			pthread_mutex_unlock(&mutex_lista1);
             return -1; // La llave ya existe
         }
         temp = temp->next;
     }
+
+	pthread_mutex_unlock(&mutex_lista1);  // Desbloquear el primer mutex después de modificar la lista
+
+	pthread_mutex_lock(&mutex_lista2);  // Bloquear el segundo mutex antes de modificar la lista
 	ptr = (struct Node *) malloc(sizeof(struct Node));
-	if (ptr == NULL) 
+	if (ptr == NULL){
+		pthread_mutex_unlock(&mutex_lista2);
+
 		return -1;
+	}
+		
 
 	ptr->V_value2 = (double *) malloc(N_value2 * sizeof(double));
 	if (ptr->V_value2 == NULL) {
 		free(ptr);
+		pthread_mutex_unlock(&mutex_lista2);
+
 		return -1;
 	}
 	strcpy(ptr->value1, value1);
@@ -52,15 +65,16 @@ int set(List *l, int key, char *value1, int N_value2, double *V_value2){
 		ptr->next = *l;
 		*l = ptr;
 	}
-	for(int i = 0; i < N_value2; i++){
-		// printf("Valor %d: %f\n", i, V_value2[i]);
-	}
+
+	
+	pthread_mutex_unlock(&mutex_lista2);  // Desbloquear el segundo mutex después de modificar la lista
 	return 0;
 }	
 
 int get(List l, int key, char *value1, int *N_value2, double *V_value2){
+	
+	pthread_mutex_lock(&mutex_lista1);
 	List aux;
-
 	aux = l;	
 	
 	while (aux!=NULL) {
@@ -70,6 +84,7 @@ int get(List l, int key, char *value1, int *N_value2, double *V_value2){
 			*N_value2 = aux->N_value2;
 			memcpy(V_value2, aux->V_value2, *N_value2*sizeof(double));
 			printf("Valor a obtener encontrado\n");
+			pthread_mutex_unlock(&mutex_lista1);
 			return 0;		// Valor encontrado
 		}
 		else{
@@ -77,6 +92,7 @@ int get(List l, int key, char *value1, int *N_value2, double *V_value2){
 		}
 	}
 	printf("Valor a obtener no encontrado\n");
+	pthread_mutex_unlock(&mutex_lista1);
 	return -1;  // Valor no encontrado
 }	
 
@@ -96,57 +112,70 @@ int printList(List l){
 	return 0;
 }	
 
-int modify(List *l, int key, char *value1, int N_value2, double *V_value2){
-	List aux;
-
-	aux = *l;
-	while (aux!=NULL) {
-		if (aux->key == key) {
-			strcpy(aux->value1, value1);
-			aux->N_value2 = N_value2;
-			memcpy(aux->V_value2, V_value2, N_value2*sizeof(double));
-			printf("Valor a modificar encontrado\n");
-			return 0;		// Valor encontrado
+	int modify(List *l, int key, char *value1, int N_value2, double *V_value2){
+		pthread_mutex_lock(&mutex_lista1);
+		List aux;
+		aux = *l;
+		while (aux!=NULL) {
+			if (aux->key == key) {
+				strcpy(aux->value1, value1);
+				aux->N_value2 = N_value2;
+				memcpy(aux->V_value2, V_value2, N_value2*sizeof(double));
+				printf("Valor a modificar encontrado\n");
+				pthread_mutex_unlock(&mutex_lista1);  // Desbloquear el mutex después de modificar la lista
+				return 0;		// Valor encontrado
+			}
+			else{
+				aux = aux->next;
+			}
 		}
-		else{
-			aux = aux->next;
-		}
+		printf("Error: valor a modificar no encontrado\n");
+		pthread_mutex_unlock(&mutex_lista1);  // Desbloquear el mutex si no se encontró el valor
+		return -1;		// Valor no encontrado
 	}
-	printf("Error: valor a modificar no encontrado\n");
-	return -1;		// Valor no encontrado
-}
 
 int delete(List *l, int key){
 	List aux, back;
 
-	if (*l == NULL)  // Lista vacía
-		return -1;
+    pthread_mutex_lock(&mutex_lista1);  // Bloquea el mutex antes de verificar si la lista está vacía
 
-	// Primer elemento de la lista
-	if ((*l)->key == key){
-		aux = *l;
-		*l = (*l)->next;
-		free(aux->V_value2);
-		free(aux);
-		return 0;
-	}
-	
-	aux = *l;
-	back = *l;
-	while (aux!=NULL) {
-		if (aux->key == key) {
-			back->next = aux->next;
-			free (aux);
-			printf("Valor a borrar encontrado\n");
-			return 0;		// Valor encontrado
-		}
-		else {
-			back = aux;
-			aux = aux->next;
-		}
-	}
-	printf("Error: valor a borrar no encontrado\n");
-	return -1;		// Valor no encontrado
+    if (*l == NULL) {  // Lista vacía
+        pthread_mutex_unlock(&mutex_lista1);  // Desbloquea el mutex antes de salir
+        return -1;
+    }
+
+    pthread_mutex_unlock(&mutex_lista1);  // Desbloquea el mutex después de verificar si la lista está vacía
+
+    pthread_mutex_lock(&mutex_lista2);  // Bloquea el mutex antes de entrar a la sección crítica de modificación de la lista
+
+    // Primer elemento de la lista
+    if ((*l)->key == key){
+        aux = *l;
+        *l = (*l)->next;
+        free(aux->V_value2);
+        free(aux);
+        pthread_mutex_unlock(&mutex_lista2);  // Desbloquea el mutex después de modificar la lista
+        return 0;
+    }
+    
+    aux = *l;
+    back = *l;
+    while (aux != NULL) {
+        if (aux->key == key) {
+            back->next = aux->next;
+            free(aux);
+            printf("Valor a borrar encontrado\n");
+            pthread_mutex_unlock(&mutex_lista2);  // Desbloquea el mutex después de modificar la lista
+            return 0;       // Valor encontrado
+        } else {
+            back = aux;
+            aux = aux->next;
+        }
+    }
+
+    pthread_mutex_unlock(&mutex_lista2);  // Desbloquea el mutex si no se encontró el valor
+    printf("Error: valor a borrar no encontrado\n");
+    return -1;      // Valor no encontrado
 }	
 
 int inlist(List *l, int key){
